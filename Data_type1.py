5ff6d79396be372bf3db53feb551685714643baf
1tfeD-ELH <yeochangdong3@gmail.com>
2021-02-22 14:55:34 +0900

    2/22 for문 전까지

---
+++ Data_type1.py
@@ -0,0 +1,19 @@
+#Numeric 숫자형
+my_int1 = 1
+my_int2 = -2
+my_int3 = 4096
+my_float1 = 1.0
+my_float2 = 2.0
+my_float3 = 3.14
+
+#String 문자열
+my_str1 = 'a'
+my_str2 = '3.14'
+my_str3 = 'coding'
+my_str4 = "coding"
+
+#Boolean 불린
+my_bool1 = True
+my_bool2 = False
+my_bool3 = 1 < 2 #True
+my_bool4 = 1 == 2 #False
---
+++ format.py
@@ -0,0 +1,4 @@
+print('My name is %s' %'Tom')
+print('My name is {}'.format('Tom'))
+print('{} x {} = {}'.format(2, 3, 2 * 3))
+print('{1} x {0} = {2}'.format(2, 3, 2 * 3))
